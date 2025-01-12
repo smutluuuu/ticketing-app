a7f0705abb597ca12f585ad7e8fcb612ea281f26
smutluuuu <suleyman.mutlu96@gmail.com>
2025-01-13 00:05:10 +0300

    authentication implementation started

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
+
+const routers = [currentUserRouter, signinRouter, signoutRouter, signupRouter];
+
+routers.forEach((router) => {
+  app.use(router);
+});
 
 app.get("/api/users/currentuser", (req, res) => {
   res.send("Hi there !");
