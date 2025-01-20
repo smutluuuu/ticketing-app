47e25653fa29545dcc0308b947445f8bd89816ed
smutluuuu <suleyman.mutlu96@gmail.com>
2025-01-20 23:40:13 +0300

    Signup and validation normalization completed

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,18 +5,21 @@ import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 import jwt from "jsonwebtoken";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
 router.post(
   "/api/users/signup",
-  [body("email").isEmail().withMessage("Email must be valid"), body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters")],
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
   async (req: Request, res: Response): Promise<void> => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
