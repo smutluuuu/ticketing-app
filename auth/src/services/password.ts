f9a4cff3e2de396c137eddc7f55aa4027db13892
smutluuuuu <suleyman@trendersoftware.io>
2025-01-21 14:15:59 +0300

    authorization middlewares added

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -4,23 +4,6 @@ import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
-// const scryptAsync = promisify(scrypt);
-
-// export class Password {
-//   static async toHash(password: string) {
-//     const salt = randomBytes(8).toString("hex");
-//     const buf = (await scryptAsync(password, salt, 64)) as Buffer;
-
-//     return `${buf.toString("hex")}.${salt}`;
-//   }
-//   static async compare(storedPassword: string, suppliedPassword: string) {
-//     const [hashedPassword, salt] = storedPassword.split(".");
-//     const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-
-//     return buf.toString("hex") === hashedPassword;
-//   }
-// }
-
 export class PasswordManager {
   static async hashPassword(password: string): Promise<string> {
     const salt = await bcrypt.genSalt(saltRounds);
