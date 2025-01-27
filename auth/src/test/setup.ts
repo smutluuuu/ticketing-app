7d2a1388e3e137ca64927780684561fee8ed38a9
smutluuuuu <suleyman@trendersoftware.io>
2025-01-27 14:58:01 +0300

    client-side added

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+import request from "supertest";
 let mongo: any;
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 beforeAll(async () => {
   process.env.JWT_KEY = "asdf";
   mongo = await MongoMemoryServer.create();
@@ -24,3 +29,22 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+  const cookie = response.get("Set-Cookie");
+
+  if (!cookie) {
+    throw new Error("Failed to get cookie from response");
+  }
+  return cookie;
+};
