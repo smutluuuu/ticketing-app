b9eaf67ce151761da5a45489362f360397a053eb
smutluuuuu <suleyman@trendersoftware.io>
2025-01-28 17:05:13 +0300

    TDD is begin

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,51 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+
+let mongo: any;
+
+declare global {
+  var signin: () => string[];
+}
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db.collections();
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a JWT payload { id , email }
+  const payload = {
+    id: "qweqwe",
+    email: "test@test.com",
+  };
+  //Create the JWT!
+  var token = jwt.sign(payload, process.env.JWT_KEY!);
+  //Build session Object { jwt: MY_JWT }.
+  const session = { jwt: token };
+  //Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  //Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  //return a string thats the cookie with encoded data
+  return [`session=${base64}`];
+};
