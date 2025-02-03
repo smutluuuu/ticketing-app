dfd01d1c2170bbd68276efe14d43e18b93333079
smutluuuuu <suleyman@trendersoftware.io>
2025-02-03 16:22:08 +0300

    nats added

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,35 @@
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
+
+console.clear();
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  console.log("Listener connected to NATS");
+
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+    msg.ack();
+  });
+});
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
