8f7c9ba70dda4fd8b94157438c944dd74e5e1761
smutluuuu <suleyman.mutlu96@gmail.com>
2025-02-04 22:41:20 +0300

    Waiting for Event publication

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -3,12 +3,13 @@ import { Subjects } from "./subjects";
 
 interface Event {
   subject: Subjects;
+  data: any;
 }
 
-export abstract class Listener {
-  abstract subject: string;
+export abstract class Listener<T extends Event> {
+  abstract subject: T["subject"];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T["data"], msg: Message): void;
   protected ackWait = 5 * 1000;
   private client: Stan;
 
