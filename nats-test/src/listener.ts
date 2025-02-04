527d18d80f83635ece466e9ae852dbe582cb3419
smutluuuuu <suleyman@trendersoftware.io>
2025-02-04 16:49:37 +0300

    enforcing listener subjects

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,8 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { subscribe } from "diagnostics_channel";
+import { Listener } from "./events/base-listener";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -7,28 +10,12 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 });
 
 stan.on("connect", () => {
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  console.log("Listener connected to NATS");
-
   stan.on("close", () => {
     console.log("NATS connection closed!");
     process.exit();
   });
 
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
